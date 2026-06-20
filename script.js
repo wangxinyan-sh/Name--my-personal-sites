687eaadcf9afc1238b9d909faccce0a4a70f3d67
xinbuliuhen <143304336+xinbuliuhen@users.noreply.github.com>
2026-06-20 05:18:44 -0700

    6.2

--- script.js
+++ script.js
@@ -164,3 +164,11 @@ if (preview) {
         }
     });
 }
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+}
